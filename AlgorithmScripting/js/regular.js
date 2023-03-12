1146a2d887651d94a28dc7e4721a0f988da8360f
Julia <yulia.bobko@mail.ru>
2023-03-12 17:30:46 +0300

    Match Anything with Wildcard Period

--- AlgorithmScripting/js/regular.js
+++ AlgorithmScripting/js/regular.js
@@ -2,7 +2,12 @@
 // let codingRegex = /coding/; // Change this line
 // let result = extractStr.match(codingRegex); // Change this line
 
-//Find More Than the First Match
-let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /twinkle/gi; // Change this line
-let result = twinkleStar.match(starRegex); // Change this line
+// //Find More Than the First Match
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /twinkle/gi; // Change this line
+// let result = twinkleStar.match(starRegex); // Change this line
+
+//Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/gi; // Change this line
+let result = unRegex.test(exampleStr);
