49649e45f9acb52286909da9c15456a77096d26b
Julia <yulia.bobko@mail.ru>
2023-03-12 17:25:43 +0300

    Find More Than the First Match

---
+++ AlgorithmScripting/js/regular.js
@@ -0,0 +1,8 @@
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
+
+//Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
