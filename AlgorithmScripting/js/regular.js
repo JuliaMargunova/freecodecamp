6b0d1e80eabe03742ab448abc47b9af2afccd1ed
Julia <yulia.bobko@mail.ru>
2023-03-13 21:44:27 +0300

    Match Numbers and Letters of the Alphabet

--- AlgorithmScripting/js/regular.js
+++ AlgorithmScripting/js/regular.js
@@ -17,7 +17,12 @@
 // let vowelRegex = /[aeiou]/gi; // Change this line
 // let result = quoteSample.match(vowelRegex); // Change this line
 
-//Match Letters of the Alphabet
-let quoteSample = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /[a-z]/gi; // Change this line
-let result = quoteSample.match(alphabetRegex); // Change this line
+// //Match Letters of the Alphabet
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+
+//Match Numbers and Letters of the Alphabet
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex);
